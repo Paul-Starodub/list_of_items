349ae6cc323fd57d9220cf1de231f41da5f14525
Paul Starodub <amorallex@gmail.com>
2023-12-27 15:16:34 +0200

    update settings.py for postgres

--- items/settings.py
+++ items/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "items.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
